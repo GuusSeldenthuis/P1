68d11068e4d60a37dce63f1009bc0063ce7063bd
Guus Seldenthuis <guus1993@gmail.com>
2019-08-04 00:32:53 +0200

    Added basic block, blockchain and mining functionality.

--- src/main/java/P1.java
+++ src/main/java/P1.java
@@ -1,5 +1,25 @@
+import data.Block;
+import data.BlockChain;
+import miner.BlockMiner;
+
+import java.math.BigInteger;
+
 public class P1 {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        BlockChain blockChain = new BlockChain();
+
+        blockChain.addBlock(new Block(
+                "000000f7375e0c1d6d782e03cd2c10a5e62708fe415732c951aca5b7e704fa86",
+                "",
+                0,
+                0,
+                23182942,
+                1564871394));
+
+        System.out.println("Starting mining block 1...");
+        Block block1 = BlockMiner.mine(blockChain, "000000f7375e0c1d6d782e03cd2c10a5e62708fe415732c951aca5b7e704fa86", 1);
+
+
+        System.out.println("Mined first block! " + block1.toString());
     }
 }
